0646fd3cd26850da02939bcf57ca4ead427726fb
makseliProda <mustafa.akseli@prodabilisim.com>
2019-11-26 10:46:05 +0300

    added terminal.png

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # kafka-docker-python
 
 run python3+
+
+please change kafka port number yours "wurstmeister/kafka" 's port
+
+![alt text](https://raw.githubusercontent.com/makseli/kafka-docker-python/terminal.png) 
--- consumer.py
+++ consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test-topic', group_id='consumer-1', bootstrap_servers='172.17.0.1:32774')
+consumer = KafkaConsumer('test-topic', group_id='consumer-1', bootstrap_servers='172.17.0.1:32768')
 
 for message in consumer:
 #	print(msg).encode('utf-8')
