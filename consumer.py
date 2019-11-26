c07447d5e77ad2a3aca83c9ba0707500e4a76ea3
yazilim.proje <yazilim.proje.makseli@gmail.com>
2019-11-26 10:33:55 +0300

    kafka running

--- consumer.py
+++ consumer.py
@@ -1,11 +1,9 @@
 from kafka import KafkaConsumer
 
-var = 1
-while var == 1:
+consumer = KafkaConsumer('test-topic', group_id='consumer-1', bootstrap_servers='172.17.0.1:32774')
 
-    consumer = KafkaConsumer('test-topic',
-                            group_id='consumer-1',
-                            bootstrap_servers='172.17.0.1:32768')
-
-    for msg in consumer:
-        print (msg)
+for message in consumer:
+#	print(msg).encode('utf-8')
+	print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
--- producer.py
+++ producer.py
@@ -1,17 +1,29 @@
 import time
 import random
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
 producer = KafkaProducer(bootstrap_servers='172.17.0.1:32774')
 
-var = 1
-while var == 1:
+#var = 1
+#while var == 1:
 
-    num = random.randint(0, 10)
+num = random.randint(0, 10)
 
-    num_bytes = bytes(str(num), encoding='utf-8')
+num_bytes = bytes(str(num), encoding='utf-8')
 
-    producer.send('test-topic', value=num_bytes, key=num_bytes)
+is_send = producer.send('test-topic', value=num_bytes, key=num_bytes)
+# Block for 'synchronous' sends
+try:
+    record_metadata = is_send.get(timeout=10)
+except KafkaError as exc:
+    # Decide what to do if produce request failed...
+    #log.exception()
+    print(exc)
 
-    # wait 1 second
-    time.sleep(1)
+# Successful result returns assigned partition and offset
+print (record_metadata.topic)
+print (record_metadata.partition)
+print (record_metadata.offset)
+
+
